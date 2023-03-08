70a024a02222ff063bd0efae88da5426f0e6ee67
Aymanhamad2002 <aymanhamad200@gmail.com>
2023-03-08 15:42:24 +0200

    Create a function to randomly get the computer choice

---
+++ script.js
@@ -0,0 +1,6 @@
+// create a function to get the computer choice 
+function getComputerChoice(){
+    const choices = ["rock","paper","scissor"];
+    return choices[Math.floor(Math.random() * 3)];
+}
+console.log(getComputerChoice());
