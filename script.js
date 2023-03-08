7a764900e051111595f3762ddde10d56dc6dbf12
Aymanhamad2002 <aymanhamad200@gmail.com>
2023-03-08 16:08:08 +0200

    Add some function for the rock-paper-scissor-game.
    Create a function called getComputerChoice() that get random value for the computer choice.
    Create a function called playerSelection() that get user choice .
    Create a function called PlayRound() that  compare the two choices and return a winner.

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ function getComputerChoice(){
     const choices = ["rock","paper","scissor"];
     return choices[Math.floor(Math.random() * 3)];
 }
+// create a function that has no parameter to get the user choice
+function playerSelection(){
+    let choice = prompt("What is your choice: ");
+    return choice.toLowerCase();
+}
+// create a function that take a the computer and the player  choice as a prameter  and return the result
+function playRound(ComputerChoice,playerSelection){
+     if(playerSelection == ComputerChoice ){
+        return "tie";}
+
+     else if(playerSelection == "rock" && ComputerChoice == "scissor"){
+        return "You win! Rock beat scissor";
+    
+    }
+     else if(playerSelection == "scissor" && ComputerChoice == "paper"){
+        return "You win ! scissor beat paper" ;
+    }
+    else if(playerSelection == "paper" && ComputerChoice == "rock"){
+        return "You win ! paper beat rock" ;
+
+    }
+    else {
+        return ("you lose! " + ComputerChoice +"beat " + playerSelection);
+    }
+}
